3d4eb04a5a41c7a57b9f774c14c46ad4ff49b244
Stefan Schwarz <ssz@bitsbeats.com>
2019-03-11 10:53:27 +0100

    new tests

--- plugin/plugin_test.go
+++ plugin/plugin_test.go
@@ -18,27 +18,9 @@ var noContext = context.Background()
 
 const mockToken = "7535706b694c63526c6e4f5230374243"
 
+// test commit
 func TestPlugin(t *testing.T) {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/repos/foosinn/dronetest/compare/2897b31ec3a1b59279a08a8ad54dc360686327f7...8ecad91991d5da985a2a8dd97cc19029dc1c2899",
-		func(w http.ResponseWriter, r *http.Request) {
-			f, _ := os.Open("testdata/compare.json")
-			io.Copy(w, f)
-		})
-	mux.HandleFunc("/repos/foosinn/dronetest/contents/a/b/.drone.yml",
-		func(w http.ResponseWriter, r *http.Request) {
-			r.ParseForm()
-			if r.Form["ref"][0] != "8ecad91991d5da985a2a8dd97cc19029dc1c2899" {
-				w.WriteHeader(404)
-				return
-			}
-			f, _ := os.Open("testdata/a_b_.drone.yml")
-			io.Copy(w, f)
-		})
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		logrus.Debugln(r.URL)
-	})
-	ts := httptest.NewServer(mux)
+	ts := httptest.NewServer(testMux())
 	defer ts.Close()
 
 	req := &config.Request{
@@ -53,17 +35,98 @@ func TestPlugin(t *testing.T) {
 			Config:    ".drone.yml",
 		},
 	}
-
 	plugin := New(ts.URL, mockToken, false)
-	config, err := plugin.Find(noContext, req)
+	droneConfig, err := plugin.Find(noContext, req)
 	if err != nil {
 		t.Error(err)
 		return
 	}
 
-	if want, got := "CORRECT\n\n", config.Data; want != got {
+	if want, got := "CORRECT\n\n", droneConfig.Data; want != got {
 		t.Errorf("Want %q got %q", want, got)
 	}
+}
 
-	// TODO: add tests for concat
+func TestConcat(t *testing.T) {
+	ts := httptest.NewServer(testMux())
+	defer ts.Close()
+
+	req := &config.Request{
+		Build: drone.Build{
+			Before: "2897b31ec3a1b59279a08a8ad54dc360686327f7",
+			After:  "8ecad91991d5da985a2a8dd97cc19029dc1c2899",
+		},
+		Repo: drone.Repo{
+			Namespace: "foosinn",
+			Name:      "dronetest",
+			Slug:      "foosinn/dronetest",
+			Config:    ".drone.yml",
+		},
+	}
+	plugin := New(ts.URL, mockToken, true)
+	droneConfig, err := plugin.Find(noContext, req)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	if want, got := "CORRECT\n\n\n---\nCORRECT2\n", droneConfig.Data; want != got {
+		t.Errorf("Want %q got %q", want, got)
+	}
+}
+
+func TestPullRequest(t *testing.T) {
+	ts := httptest.NewServer(testMux())
+	defer ts.Close()
+
+	req := &config.Request{
+		Build: drone.Build{
+			Fork: "octocat/dronetest",
+			Ref:  "refs/pull/3/head",
+		},
+		Repo: drone.Repo{
+			Namespace: "foosinn",
+			Name:      "dronetest",
+			Slug:      "foosinn/dronetest",
+			Config:    ".drone.yml",
+		},
+	}
+	plugin := New(ts.URL, mockToken, true)
+	droneConfig, err := plugin.Find(noContext, req)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	if want, got := "CORRECT2\n", droneConfig.Data; want != got {
+		t.Errorf("Want %q got %q", want, got)
+	}
+}
+
+func testMux() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/repos/foosinn/dronetest/compare/2897b31ec3a1b59279a08a8ad54dc360686327f7...8ecad91991d5da985a2a8dd97cc19029dc1c2899",
+		func(w http.ResponseWriter, r *http.Request) {
+			f, _ := os.Open("testdata/compare.json")
+			io.Copy(w, f)
+		})
+	mux.HandleFunc("/repos/foosinn/dronetest/contents/a/b/.drone.yml",
+		func(w http.ResponseWriter, r *http.Request) {
+			f, _ := os.Open("testdata/a_b_.drone.yml.json")
+			io.Copy(w, f)
+		})
+	mux.HandleFunc("/repos/foosinn/dronetest/contents/.drone.yml",
+		func(w http.ResponseWriter, r *http.Request) {
+			f, _ := os.Open("testdata/.drone.yml.json")
+			io.Copy(w, f)
+		})
+	mux.HandleFunc("/repos/foosinn/dronetest/pulls/3/files",
+		func(w http.ResponseWriter, r *http.Request) {
+			f, _ := os.Open("testdata/pull_3_files.json")
+			io.Copy(w, f)
+		})
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		logrus.Info(r.URL)
+	})
+	return mux
 }
