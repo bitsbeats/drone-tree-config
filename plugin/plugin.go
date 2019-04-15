d2eaa7b97fdbbaddfb13c37d4a544eb8e7dc9a79
Stefan Schwarz <ssz@bitsbeats.com>
2019-04-15 09:57:31 +0200

    support cronjobs + better logging

--- plugin/plugin.go
+++ plugin/plugin.go
@@ -16,6 +16,7 @@ import (
 	"github.com/sirupsen/logrus"
 	"golang.org/x/oauth2"
 	"gopkg.in/yaml.v2"
+	"github.com/google/uuid"
 )
 
 // New creates a drone plugin
@@ -38,24 +39,25 @@ type (
 		Name string `yaml:"name"`
 		Kind string `yaml:"kind"`
 	}
+
+	request struct {
+		*config.Request
+		UUID uuid.UUID
+		Client *github.Client
+	}
 )
 
 var dedupRegex = regexp.MustCompile(`(?ms)(---[\s]*){2,}`)
 
-func (p *plugin) Find(ctx context.Context, req *config.Request) (*drone.Config, error) {
-	logrus.Infof("--- STARTED ---- %s ---", req.Build.Ref)
-	defer logrus.Infof("--- FINISHED --- %s ---", req.Build.Ref)
+func (p *plugin) Find(ctx context.Context, droneRequest *config.Request) (*drone.Config, error) {
+	uuid := uuid.New()
+	logrus.Infof("%s %s/%s started", uuid, droneRequest.Repo.Namespace, droneRequest.Repo.Name, )
+	defer logrus.Infof("%s finished", uuid)
 
-	// log
-	logrus.Debugf("Build: %+v", req.Build)
-	logrus.Debugf("Repo: %+v", req.Repo)
-
-	// creates a github transport that authenticates
+	// connect to github
 	trans := oauth2.NewClient(ctx, oauth2.StaticTokenSource(
 		&oauth2.Token{AccessToken: p.token},
 	))
-
-	// connect with github
 	var client *github.Client
 	if p.server == "" {
 		client = github.NewClient(trans)
@@ -63,19 +65,131 @@ func (p *plugin) Find(ctx context.Context, req *config.Request) (*drone.Config, 
 		var err error
 		client, err = github.NewEnterpriseClient(p.server, p.server, trans)
 		if err != nil {
-			logrus.Errorf("Unable to connect to Github: '%v', server: '%s'", err, p.server)
+			logrus.Errorf("%s Unable to connect to Github: '%v'", uuid, err)
 			return nil, err
 		}
 	}
 
+	req := request{droneRequest, uuid, client}
+
 	// get changed files
-	changedFiles, err := p.getGithubChanges(ctx, req, client)
+	changedFiles, err := p.getGithubChanges(ctx, &req)
 	if err != nil {
 		return nil, err
 	}
 
-	// collect drone.yml files
+	// get drone.yml for changed files or all of them if no changes/cron
 	configData := ""
+	if changedFiles != nil {
+		configData, err = p.getGithubConfigData(ctx, &req, changedFiles)
+	} else {
+		configData, err = p.getAllConfigData(ctx, &req, "/")
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	// no file found
+	if configData == "" {
+		return nil, errors.New("did not find a .drone.yml")
+	}
+
+	// cleanup
+	configData = strings.ReplaceAll(configData, "...", "")
+	configData = string(dedupRegex.ReplaceAll([]byte(configData), []byte("---")))
+
+	return &drone.Config{Data: configData}, nil
+}
+
+// get repo changes
+func (p *plugin) getGithubChanges(ctx context.Context, req *request) ([]string, error) {
+	var changedFiles []string
+
+	if req.Build.Trigger == "@cron" {
+		// cron jobs trigger a full build
+		changedFiles = []string{}
+	} else if strings.HasPrefix(req.Build.Ref, "refs/pull/") {
+		// use pullrequests api to get changed files
+		pullRequestID, err := strconv.Atoi(strings.Split(req.Build.Ref, "/")[2])
+		if err != nil {
+			logrus.Errorf("%s unable to get pull request id %v", req.UUID, err)
+			return nil, err
+		}
+		opts := github.ListOptions{}
+		files, _, err := req.Client.PullRequests.ListFiles(ctx, req.Repo.Namespace, req.Repo.Name, pullRequestID, &opts)
+		if err != nil {
+			logrus.Errorf("%s unable to fetch diff for Pull request %v", req.UUID, err)
+			return nil, err
+		}
+		for _, file := range files {
+			changedFiles = append(changedFiles, *file.Filename)
+		}
+	} else {
+		// use diff to get changed files
+		before := req.Build.Before
+		if before == "0000000000000000000000000000000000000000" || before == "" {
+			before = fmt.Sprintf("%s~1", req.Build.After)
+		}
+		changes, _, err := req.Client.Repositories.CompareCommits(ctx, req.Repo.Namespace, req.Repo.Name, before, req.Build.After)
+		if err != nil {
+			logrus.Errorf("%s unable to fetch diff: '%v'", req.UUID, err)
+			return nil, err
+		}
+		for _, file := range changes.Files {
+			changedFiles = append(changedFiles, *file.Filename)
+		}
+	}
+
+	if len(changedFiles) > 0 {
+		changedList := strings.Join(changedFiles, "\n  ")
+		logrus.Debugf("%s changed files: \n  %s", req.UUID, changedList)
+	} else {
+		logrus.Warnf("%s no changed files, rebuilding all", req.UUID)
+		return nil, nil
+	}
+	return changedFiles, nil
+}
+
+// get the contents of a file on github, if the file is not found throw an error
+func (p *plugin) getGithubFile(ctx context.Context, req *request, file string) (content string, err error) {
+	logrus.Debugf("%s checking %s/%s %s", req.UUID, req.Repo.Namespace, req.Repo.Name, file)
+	ref := github.RepositoryContentGetOptions{Ref: req.Build.After}
+	data, _, _, err := req.Client.Repositories.GetContents(ctx, req.Repo.Namespace, req.Repo.Name, file, &ref)
+	if data == nil {
+		err = fmt.Errorf("failed to get %s: is not a file", file)
+	}
+	if err != nil {
+		return "", err
+	}
+	return data.GetContent()
+}
+
+// download and validate a drone.yml
+func (p *plugin) getGithubDroneConfig(ctx context.Context, req *request, file string) (configData string, critical bool, err error) {
+	fileContent, err := p.getGithubFile(ctx, req, file)
+	if err != nil {
+		logrus.Debugf("%s skipping: unable to load file: %s %v", req.UUID, file, err)
+		return "", false, err
+	}
+
+	// validate fileContent, exit early if an error was found
+	dc := droneConfig{}
+	err = yaml.Unmarshal([]byte(fileContent), &dc)
+	if err != nil {
+		logrus.Errorf("%s skipping: unable do parse yml file: %s %v", req.UUID, file, err)
+		return "", true, err
+	}
+	if dc.Name == "" || dc.Kind == "" {
+		logrus.Errorf("%s skipping: missing 'kind' or 'name' in %s.", req.UUID, file)
+		return "", true, err
+	}
+
+	return fileContent, false, nil
+}
+
+func (p *plugin) getGithubConfigData(ctx context.Context, req *request, changedFiles []string) (configData string, err error){
+	// collect drone.yml files
+	configData = ""
 	cache := map[string]bool{}
 	for _, file := range changedFiles {
 		if !strings.HasPrefix(file, "/") {
@@ -97,110 +211,66 @@ func (p *plugin) Find(ctx context.Context, req *config.Request) (*drone.Config, 
 				cache[file] = true
 			}
 
-			// check file on github and append
-			fileContent, err := p.getGithubFile(ctx, req, client, file)
+			// download file from git
+			fileContent, critical, err := p.getGithubDroneConfig(ctx, req, file)
 			if err != nil {
-				logrus.Debugf("Skipping: unable to load file: %s %v", file, err)
+				if critical {
+					return "", err
+				}
 				continue
 			}
 
-			// validate fileContent, exit early if an error was found
-			dc := droneConfig{}
-			err = yaml.Unmarshal([]byte(fileContent), &dc)
-			if err != nil {
-				logrus.Errorf("Skipping: unable do parse yaml file: %s %v", file, err)
-				return nil, err
-			}
-			if dc.Name == "" || dc.Kind == "" {
-				logrus.Errorf("Skipping: missing 'kind' or 'name' in %s.", file)
-				return nil, err
-			}
-
 			// append
-			logrus.Infof("Found %s/%s %s", req.Repo.Namespace, req.Repo.Name, file)
-			if configData != "" {
-				configData += "\n---\n"
-			}
-			configData += fileContent + "\n"
+			configData = p.droneConfigAppend(configData, fileContent)
+			logrus.Infof("%s found %s/%s %s", req.UUID, req.Repo.Namespace, req.Repo.Name, file)
 			if !p.concat {
-				logrus.Info("Concat is disabled. Using just first .drone.yaml.")
+				logrus.Infof("%s concat is disabled. Using just first .drone.yml.", req.UUID)
 				break
 			}
 		}
 	}
-
-	// no file found
-	if configData == "" {
-		return nil, errors.New("Did not find a .drone.yml")
-	}
-
-	// cleanup
-	configData = strings.ReplaceAll(configData, "...", "")
-	configData = string(dedupRegex.ReplaceAll([]byte(configData), []byte("---")))
-
-	return &drone.Config{Data: configData}, nil
+	return configData, nil
 }
 
-// get repo changes
-func (p *plugin) getGithubChanges(ctx context.Context, req *config.Request, client *github.Client) ([]string, error) {
-	changedFiles := []string{}
-
-	if strings.HasPrefix(req.Build.Ref, "refs/pull/") {
-		// use pullrequests api to get changed files
-		pullRequestID, err := strconv.Atoi(strings.Split(req.Build.Ref, "/")[2])
-		if err != nil {
-			logrus.Errorf("Unable to get pull request id: %v", err)
-			return nil, err
-		}
-		opts := github.ListOptions{}
-		files, _, err := client.PullRequests.ListFiles(ctx, req.Repo.Namespace, req.Repo.Name, pullRequestID, &opts)
-		if err != nil {
-			logrus.Errorf("Unable to fetch diff for Pull request: %v", err)
-			return nil, err
-		}
-		for _, file := range files {
-			changedFiles = append(changedFiles, *file.Filename)
-		}
-	} else {
-		// use diff to get changed files
-		before := req.Build.Before
-		if before == "0000000000000000000000000000000000000000" {
-			before = fmt.Sprintf("%s~1", req.Build.After)
-		}
-		changes, _, err := client.Repositories.CompareCommits(ctx, req.Repo.Namespace, req.Repo.Name, before, req.Build.After)
-		if err != nil {
-			logrus.Errorf("Unable to fetch diff: '%v', server: '%s'", err, p.server)
-			return nil, err
-		}
-		for _, file := range changes.Files {
-			changedFiles = append(changedFiles, *file.Filename)
-		}
-	}
-
-	if len(changedFiles) > 0 {
-		changedList := strings.Join(changedFiles, "\n  ")
-		logrus.Debugf("Changed files: \n  %s", changedList)
-	} else {
-		logrus.Warn("No changed files found!")
-		return nil, errors.New("No changed files found")
-	}
-	return changedFiles, nil
-}
-
-// get the contents of a file on github, if the file is not found throw an error
-func (p *plugin) getGithubFile(ctx context.Context, req *config.Request, client *github.Client, file string) (string, error) {
-	logrus.Infof("Testing %s/%s %s", req.Repo.Namespace, req.Repo.Name, file)
-
+// search for all or fist drone.yml in repo
+func (p *plugin) getAllConfigData(ctx context.Context, req *request, dir string) (configData string, err error) {
 	ref := github.RepositoryContentGetOptions{Ref: req.Build.After}
-	data, _, _, err := client.Repositories.GetContents(ctx, req.Repo.Namespace, req.Repo.Name, file, &ref)
+	_, ls, _, err := req.Client.Repositories.GetContents(ctx, req.Repo.Namespace, req.Repo.Name, dir, &ref)
 	if err != nil {
 		return "", err
 	}
 
-	content, err := data.GetContent()
-	if err != nil {
-		return "", err
+	// check recursivly for drone.yml
+	configData = ""
+	for _, f := range ls {
+		var fileContent string
+		if *f.Type == "dir" {
+			fileContent, _ = p.getAllConfigData(ctx, req, *f.Path)
+		} else if *f.Type == "file" && *f.Name == req.Repo.Config {
+			var critical bool
+			fileContent, critical, err = p.getGithubDroneConfig(ctx, req, *f.Path)
+			if critical {
+				return "", err
+			}
+		}
+		// append
+		configData = p.droneConfigAppend(configData, fileContent)
+		if !p.concat {
+			logrus.Infof("%s concat is disabled. Using just first .drone.yml.", req.UUID)
+			break
+		}
 	}
 
-	return content, nil
+	return configData, nil
+
+}
+
+func (p *plugin) droneConfigAppend(droneConfig string, appends... string) string {
+	for _, a := range appends {
+		if droneConfig != "" {
+			droneConfig += "\n---\n"
+		}
+		droneConfig += a + "\n"
+	}
+	return droneConfig
 }
--- plugin/plugin_test.go
+++ plugin/plugin_test.go
@@ -103,8 +103,70 @@ func TestPullRequest(t *testing.T) {
 	}
 }
 
+
+func TestCron(t *testing.T) {
+	ts := httptest.NewServer(testMux())
+	defer ts.Close()
+
+	req := &config.Request{
+		Build: drone.Build{
+			After:  "8ecad91991d5da985a2a8dd97cc19029dc1c2899",
+			Trigger: "@cron",
+		},
+		Repo: drone.Repo{
+			Namespace: "foosinn",
+			Name:      "dronetest",
+			Slug:      "foosinn/dronetest",
+			Config:    ".drone.yml",
+		},
+	}
+	plugin := New(ts.URL, mockToken, false)
+	droneConfig, err := plugin.Find(noContext, req)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	if want, got := "kind: pipeline\nname: default\n\nsteps:\n- name: frontend\n  image: node\n  commands:\n  - npm install\n  - npm test\n\n- name: backend\n  image: golang\n  commands:\n  - go build\n  - go test\n\n", droneConfig.Data; want != got {
+		t.Errorf("Want %q got %q", want, got)
+	}
+}
+
+func TestCronConcat(t *testing.T) {
+	ts := httptest.NewServer(testMux())
+	defer ts.Close()
+
+	req := &config.Request{
+		Build: drone.Build{
+			After:  "8ecad91991d5da985a2a8dd97cc19029dc1c2899",
+			Trigger: "@cron",
+		},
+		Repo: drone.Repo{
+			Namespace: "foosinn",
+			Name:      "dronetest",
+			Slug:      "foosinn/dronetest",
+			Config:    ".drone.yml",
+		},
+	}
+	plugin := New(ts.URL, mockToken, true)
+	droneConfig, err := plugin.Find(noContext, req)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	if want, got := "kind: pipeline\nname: default\n\nsteps:\n- name: frontend\n  image: node\n  commands:\n  - npm install\n  - npm test\n\n- name: backend\n  image: golang\n  commands:\n  - go build\n  - go test\n\n\n---\nkind: pipeline\nname: default\n\nsteps:\n- name: build\n  image: golang\n  commands:\n  - go build\n  - go test -short\n\n- name: integration\n  image: golang\n  commands:\n  - go test -v\n\n\n", droneConfig.Data; want != got {
+		t.Errorf("Want %q got %q", want, got)
+	}
+}
+
 func testMux() *http.ServeMux {
 	mux := http.NewServeMux()
+	mux.HandleFunc("/repos/foosinn/dronetest/contents/",
+		func(w http.ResponseWriter, r *http.Request) {
+			f, _ := os.Open("testdata/root.json")
+			_, _ = io.Copy(w, f)
+		})
 	mux.HandleFunc("/repos/foosinn/dronetest/compare/2897b31ec3a1b59279a08a8ad54dc360686327f7...8ecad91991d5da985a2a8dd97cc19029dc1c2899",
 		func(w http.ResponseWriter, r *http.Request) {
 			f, _ := os.Open("testdata/compare.json")
@@ -125,8 +187,18 @@ func testMux() *http.ServeMux {
 			f, _ := os.Open("testdata/pull_3_files.json")
 			_, _ = io.Copy(w, f)
 		})
+	mux.HandleFunc("/repos/foosinn/dronetest/contents/afolder/.drone.yml",
+		func(w http.ResponseWriter, r *http.Request) {
+			f, _ := os.Open("testdata/afolder_.drone.yml.json")
+			_, _ = io.Copy(w, f)
+		})
+	mux.HandleFunc("/repos/foosinn/dronetest/contents/afolder",
+		func(w http.ResponseWriter, r *http.Request) {
+			f, _ := os.Open("testdata/afolder.json")
+			_, _ = io.Copy(w, f)
+		})
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		logrus.Info(r.URL)
+		logrus.Errorf("Url not found: %s", r.URL)
 	})
 	return mux
 }
